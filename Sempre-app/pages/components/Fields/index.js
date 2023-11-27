fcfc730dfb7a70a737105c6d355dbcdf931e22a9
Antony Henrique Bresolin <40371352+AntonyBresolin@users.noreply.github.com>
2023-11-27 18:38:33 -0300

    c2-utilizar-back-end-criar-novo-campo
    integrate the backend in the frontend ecosystem.
    add a method listAllFields. that method use the keyStart subscribe in the createField method.

--- Sempre-app/pages/components/Fields/index.js
+++ Sempre-app/pages/components/Fields/index.js
@@ -1,8 +1,26 @@
-import React, { Image, Text, TouchableOpacity } from "react-native";
+import React, { useEffect, useState } from 'react';
+import { View, TouchableOpacity, Text, Image, StyleSheet } from 'react-native';
+import { listAllFields } from '../../../backend/listAllFields'; // Importe a função listAllFields
+
 import styles from "./styles";
 import Add_Item from "../../../assets/images/icons/New_List.png";
+import experiment from "../../../assets/images/icons/Experiment.png";
+
+
+
 
 export default function Listas({ navigation }) {
+  const [fields, setFields] = useState([]);
+
+  useEffect(() => {
+    const fetchFields = async () => {
+      const allFields = await listAllFields();
+      setFields(allFields);
+    };
+
+    fetchFields();
+  }, []);
+
   return (
     <>
       <TouchableOpacity onPress={() => navigation.navigate('Fields_add_field')}>
@@ -10,6 +28,19 @@ export default function Listas({ navigation }) {
         <Text style={styles.primeiro_item}>Criar um novo campo</Text>
       </TouchableOpacity>
       <Text style={styles.borda} />
+
+      <View>
+        {fields.map(field => (
+          <TouchableOpacity
+            key={field.key}
+            style={styles.botao}
+            onPress={() => onSelectField(field)}
+          >
+            <Image source={experiment} style={styles.imagem} />
+            <Text style={styles.nome_Elemento}>{field.nome}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
 
       {/* TODO - Listar os campos cadastrados*/}
 
