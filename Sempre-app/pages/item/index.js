f1adfe18fc56c24ef40664eed2b439f7c4406950
Antony Henrique Bresolin <40371352+AntonyBresolin@users.noreply.github.com>
2023-12-04 00:41:02 -0300

    C6 adicionar itens no experimento (#6)
    * c6 - add component to create itens
    
    add new's libraries to use Ionic icon's
    
    * c6 - connected and use the backend
    
    create the method and the verifications to add itens in the experiment.

---
+++ Sempre-app/pages/item/index.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { SafeAreaView, Text, TextInput, TouchableOpacity, View, Alert } from 'react-native';
+import styles from "./styles";
+import { generateAndAddItems } from '../../backend/generateAndAddItems';
+import { useRoute } from '@react-navigation/native';
+
+
+const Item = () => {
+    const [itemAmount, setItemAmount] = useState("");
+    const [itemStart, setItemStart] = useState("");
+    const route = useRoute();
+    const fieldKey = route.params.fieldKey;
+    const experimentId = route.params.experimentId;
+    const experimentName = route.params.experimentName;
+
+    const handleAddItems = async () => {
+        const startNumber = parseInt(itemStart, 10);
+        const itemCount = parseInt(itemAmount, 10);
+
+        if (isNaN(startNumber) || isNaN(itemCount)) {
+            Alert.alert("Erro", "Por favor, insira números válidos.");
+            return;
+        }
+
+        try {
+            await generateAndAddItems(fieldKey, experimentId, startNumber, itemCount);
+            Alert.alert("Sucesso", `${itemCount} itens adicionados ao experimento.`);
+            setItemAmount("");
+            setItemStart("");
+        } catch (e) {
+            Alert.alert("Erro", "Não foi possível adicionar os itens ao experimento.");
+        }
+    };
+
+
+    return (
+        <>
+            <View >
+                <Text style={styles.textTitle}>Adicionar um novo item ao Experimento</Text>
+                <Text style={styles.textName}>{experimentName}</Text>
+            </View>
+            <View style={styles.borda} />
+
+            <SafeAreaView>
+                <Text style={styles.text}>Insira a quantidade de itens</Text>
+                <TextInput
+                    placeholderTextColor={"#CECECE"}
+                    placeholder="10"
+                    style={styles.input}
+                    keyboardType="numeric"
+                    value={itemAmount}
+                    onChangeText={setItemAmount}
+                />
+                <Text style={styles.text}>Insira o inicio</Text>
+                <TextInput
+                    placeholderTextColor={"#CECECE"}
+                    placeholder="10100"
+                    style={styles.input}
+                    keyboardType="numeric"
+                    value={itemStart}
+                    onChangeText={setItemStart}
+                />
+                <TouchableOpacity style={styles.botao} onPress={handleAddItems}>
+                    <Text style={styles.text_botao}>Adicionar</Text>
+                </TouchableOpacity>
+            </SafeAreaView>
+        </>
+    );
+};
+
+export default Item;
